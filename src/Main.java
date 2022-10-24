5cda3c42a5fbbed5fbcace02b34a70950b54dad8
https://github.com/mall24/definicion-y-reaccion-de-variables <mayerlyospina2005@gmail.com>
2022-10-24 16:33:36 -0500

    trabajo de las variables sobre los 5 trabajadores

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+    public class Main {
+
+        public static void main(String[] args){
+            Scanner Sc = new Scanner(System.in);
+            String nombre;
+            int  dias;
+            int sueldo;
+            String nombre2;
+            int  dias2;
+            int sueldo2;
+            String nombre3;
+            int  dias3;
+            int sueldo3;
+            String nombre4;
+            int  dias4;
+            int sueldo4;
+            String nombre5;
+            int  dias5;
+            int sueldo5;
+            sueldo = 800000;
+            double horas;
+            System.out.println("señor usuario  por favor escriba su nombre ");
+            nombre = Sc.nextLine();
+            System.out.println("me regalas tu edad por favor ");
+            dias = Sc.nextInt();
+            System.out.println("señor usuario horas trabajadas");
+            horas = Sc.nextByte();
+            System.out.println("su nombre es " +nombre+ " bien y su edad es" +dias+ " y sus horas son" +horas+ "");
+            System.out.println("y su sueldo es " +sueldo+ "muchas gracias por ingresar su informacion");
+
+            System.out.println("señor  usuario por favor escriba su nombre");
+            nombre2 = Sc.nextLine();
+            System.out.println("me regalas tu edad por favor");
+            dias2 = Sc.nextInt();
+            System.out.println("señor usuario horas trabajadas");
+            sueldo2 = Sc.nextByte();
+            System.out.println("su nombre es " +nombre2+ " bien y su edad es" +dias2+ " y sus horas son" );
+            System.out.println("y su sueldo es " +sueldo2+ "muchas gracias por ingresar su informacion ");
+
+            System.out.println(" señor usuario por favor escriba su nombre");
+            nombre3 = Sc.nextLine();
+            System.out.println("me regalas tu edad por favor");
+            dias3 = Sc.nextInt();
+            System.out.println("señor usuario horas trabajadas");
+            sueldo3 = Sc.nextByte();
+            System.out.println("su nombre es " +nombre3+ " bien y su edad es" +dias3+ " y sus horas son" );
+            System.out.println("y su sueldo es " +sueldo3+ "muchas gracias por ingresar su informacion ");
+
+            System.out.println("señor usuario por favor escriba su nombre");
+            nombre4 = Sc.nextLine();
+            System.out.println("me regalas tu edad por favor");
+            dias4 = Sc.nextInt();
+            System.out.println("señor usuario  horas trabajadas");
+            sueldo4 = Sc.nextByte();
+            System.out.println("su nombre es " +nombre4+ " bien y su edad es" +dias4+ " y sus horas son" );
+            System.out.println("y su sueldo es " +sueldo4+ "muchas gracias por ingresar su informacion ");
+
+            System.out.println("señor usuario por favor escriba su nombre");
+            nombre5 = Sc.nextLine();
+            System.out.println("me regalas  tu edad por favor");
+            dias5 = Sc.nextInt();
+            System.out.println("señor usuario horas trabajadas");
+            sueldo5 = Sc.nextByte();
+            System.out.println("su nombre es " +nombre5+ " bien y su edad es" +dias5+ " y sus horas son" );
+            System.out.println("y su sueldo es " +sueldo5+ "muchas gracias por ingresar su informacion");
+        }
+    }
